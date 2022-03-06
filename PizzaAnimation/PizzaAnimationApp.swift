fd81e2f4644c9060c4b24010ee29cb089ded50c1
rahulon12 <rahulon@icloud.com>
2022-03-06 18:46:00 -0500

    Initial Commit

---
+++ PizzaAnimation/PizzaAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaAnimationApp.swift
+//  PizzaAnimation
+//
+//  Created by Rahul Narayanan on 3/6/22.
+//
+
+import SwiftUI
+
+@main
+struct PizzaAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
